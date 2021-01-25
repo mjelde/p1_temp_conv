550f1e97184fccc79bebad195f099df117d89b11
Begemottt <55757703+Begemottt@users.noreply.github.com>
2021-01-24 17:34:03 -0800

    New update!

--- group_project_1/index.php
+++ group_project_1/index.php
@@ -80,9 +80,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Only need to run this code if we 
     } // end if isset
 }
 
-
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -93,6 +90,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Only need to run this code if we 
     <title>Temperature Converter</title>
 
     <link rel="stylesheet" href="css/styles.css">
+    <!-- Google font: Robot Slab -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
@@ -100,14 +101,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Only need to run this code if we 
         <form action="" method="POST">
             <h1>Temperature Converter</h1>
 
-            <label for="temp">Enter Temperature: </label>
-            <input type="text" name="temp" value="<?php if (isset($temp)) {
-                                                        echo $temp;
-                                                    } ?>">
+            <p><label for="temp">Enter Temperature: </label>
+                <input type="text" name="temp" value="<?php if (isset($temp)) {
+                                                            echo $temp;
+                                                        } ?>">
+            </p>
             <p class="error"><?php echo $temp_error; ?></p>
-            <label for="unit_from">Select Unit: </label>
+            <p><label for="unit_from">Select Unit</label></p>
             <select name="unit_from" id="unit_from">
-                <option value="">Select Unit</option>
+                <option value="">Curent Unit:</option>
                 <option value="fahrenheit" <?php if ($unit_from === 'fahrenheit') {
                                                 echo 'selected = selected';
                                             } ?>>Fahrenheit</option>
@@ -119,7 +121,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Only need to run this code if we 
                                         } ?>>Kelvin</option>
             </select>
             <p class="error"><?php echo $unit_from_error; ?></p>
-            <label for="unit_to">Convert to: </label>
+            <p><label for="unit_to">Converting To</label></p>
             <select name="unit_to" id="unit_to">
                 <option value="">Select Unit</option>
                 <option value="fahrenheit" <?php if ($unit_to === 'fahrenheit') {
@@ -142,7 +144,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Only need to run this code if we 
         <?php
         if ($result != "") { // If the result ISN'T "", then we know we have an answer and can display it!
             $result_f = number_format($result, 2); // Cut off the all but the first two decimal places
-            echo "<article>";
+            echo "<article class='result_box'>";
             echo "<p>Starting Temperature: <em>{$temp}&deg; {$unit_from}</em></p>";
             echo "<p>Desired Unit: <em>{$unit_to}</em></p>";
             echo "<p class='answer'>The Answer is: {$result_f}&deg;{$unit_to_short}! </p>";
